26f0c2581355e757972a0f8b73fee70df27fadfc
Simon Colmer <simon_colmer@hotmail.com>
2013-09-09 15:39:05 +0100

    Initial Commit

---
+++ ImageInfo.cs
@@ -0,0 +1,8 @@
+﻿namespace RoadKill.Plugins.Kittens
+{
+    internal class ImageInfo
+    {
+        public int Height { get; set; }
+        public int Width { get; set; }
+    }
+}
---
+++ KittensPlugin.cs
@@ -0,0 +1,100 @@
+﻿using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using Roadkill.Core.Configuration;
+using Roadkill.Core.Database;
+using System.Text.RegularExpressions;
+
+namespace RoadKill.Plugins.Kittens
+{
+    public class KittensPlugin : Roadkill.Core.Plugins.CustomVariablePlugin
+    {
+        public KittensPlugin(ApplicationSettings applicationSettings, IRepository repository)
+            : base(applicationSettings, repository)
+        {
+        }
+
+        public override string Id
+        {
+            get { return "KittensPlugin"; }
+        }
+
+        public override string Name
+        {
+            get { return "KittensPlugin"; }
+        }
+
+        public override string Description
+        {
+            get { return "KittensPlugin"; }
+        }
+
+        public override string AfterParse(string html)
+        {
+            const string placeKitten = "http://placekitten.com/g/{0}/{1}";
+            const string regex = @"<img\s*src=""(?<src>.+?)""";
+            const RegexOptions myRegexOptions = RegexOptions.IgnoreCase | RegexOptions.Multiline;
+            Regex myRegex = new Regex(regex, myRegexOptions);
+
+            foreach (Match myMatch in myRegex.Matches(html))
+            {
+                if (myMatch.Success)
+                {
+                    Group group = myMatch.Groups["src"];
+                    string fileName = GetFileName(group.Value);
+
+                    string path = Path.Combine(ApplicationSettings.AttachmentsDirectoryPath, fileName);
+
+                    ImageInfo imageSize = GetImageSize(path);
+                    string url = string.Format(placeKitten, imageSize.Width, imageSize.Height);
+
+                    html = html.Replace(group.Value, url);
+                }
+            }
+
+            return html;
+        }
+
+
+        public string GetFileName(string fileName)
+        {
+            fileName = fileName.Replace("&#x2E;", ".");
+            fileName = fileName.Replace("&#x2F;", "/");
+
+            if (fileName.StartsWith("/Attachments/"))
+            {
+                fileName = fileName.Substring("/Attachments/".Length);
+            }
+
+            return fileName;
+        }
+
+        private static readonly Dictionary<string, ImageInfo> ImageInfo = new Dictionary<string, ImageInfo>();
+        private static ImageInfo GetImageSize(string path)
+        {
+            ImageInfo result;
+
+            if (ImageInfo.ContainsKey(path.ToLower()))
+            {
+                result = ImageInfo[path.ToLower()];
+            }
+            else
+            {
+                result = new ImageInfo();
+
+                using (FileStream file = new FileStream(path, FileMode.Open, FileAccess.Read))
+                {
+                    using (Image image = Image.FromStream(file, useEmbeddedColorManagement: false, validateImageData: false))
+                    {
+                        result.Width = (int)image.PhysicalDimension.Width;
+                        result.Height = (int)image.PhysicalDimension.Height;
+                    }
+                }
+
+                ImageInfo.Add(path.ToLower(), result);
+            }
+
+            return result;
+        }
+    }
+}
